a4b232d0f5a3eb2bd59fcea3a1c861189765737a
Ted Huang <131336830+EnjoyerGG@users.noreply.github.com>
2025-01-24 01:18:53 +0800

    Update 54.🟡螺旋矩阵.java

--- LeetCode/二维数组花式遍历/54.🟡螺旋矩阵.java
+++ LeetCode/二维数组花式遍历/54.🟡螺旋矩阵.java
@@ -31,6 +31,40 @@ class Soulution{
         for(int j=left_bound; j<=right_bound; j++){
           res.add(matrix[upper_bound][j]);
         }
+        //上边界下移
+        upper_bound++;
+      }
+
+      if(left_bound<=right_bound){
+        //在右侧从上往下遍历
+        for(int i=upper_bound; i<=lower_bound; i++){
+          res.add(matrix[i][right_bound]);
+        }
+        //右边界左移
+        right_bound--;
+      }
+
+      if(upper_bound<=lower_bound){
+        //在底部从右向左遍历
+        for(int j=right_bound; j>=left_bound; j--){
+          res.add(matrix[lower_bound][j]);
+        }
+        //下边界上移
+        lower_bound--;
+      }
+
+      if(left_bound<=right_bound){
+        //在左侧从下向上遍历
+        for(int i=lower_bound; i>=upper_bound; i--){
+          res.add(matrix[i][left_bound]);
+        }
+        //左边界右移
+        left_bound++;
+      }
+    }
+    return res;
+  }
+}
 
 
 
